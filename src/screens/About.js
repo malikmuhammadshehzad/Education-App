61f8163fb4cea1054a4bb042b2231f8a03413eb3
malikmuhammadshehzad <malikshehzad1534@gmail.com>
2023-08-27 14:56:54 +0500

    first commit

---
+++ src/screens/About.js
@@ -0,0 +1,147 @@
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import React from "react";
+import { useFonts, WorkSans_400Regular } from "@expo-google-fonts/work-sans";
+import { Nunito_700Bold } from "@expo-google-fonts/nunito";
+import AppLoading from "expo-app-loading";
+
+const About = () => {
+  let [fontsLoaded] = useFonts({
+    WorkSans_400Regular,
+    Nunito_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    <AppLoading />;
+  }
+
+  return (
+    <View style={styles.aboutContainer}>
+      <Text style={styles.mainHeader}> Muhammad Shehzad </Text>
+      <Text style={styles.paraStyle}> I am a App developer 😀 </Text>
+
+      <View>
+        <Image
+          style={styles.imgStyle}
+          source={require('../api/Shehzad.png')}
+        />
+      </View>
+
+      <View style={styles.aboutLayout}>
+        <Text style={styles.aboutSubHeader}> About me </Text>
+        <Text style={[styles.paraStyle, styles.aboutPara]}>
+          🚀 Front-End Maestro | 🌐 Web Wizard | 🛠️ Robo-Geek 🎓 Army Public College Alumnus | 🚴 Cycling Enthusiast Crafting Digital Experiences | #CodeWarlock 🧙‍♂️ #AppArchitect 📱
+        </Text>
+      </View>
+
+      <Text style={styles.mainHeader}> Follow me on Social Network </Text>
+
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() =>
+            Linking.openURL("https://www.instagram.com/malikshehzad1534/")
+          }>
+          <Image
+            style={styles.iconStyle}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/2111/2111463.png",
+            }}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.linkedin.com/in/malikmuhammadshehzad/"
+            )
+          }>
+          <Image
+            style={styles.iconStyle}
+            source={require('../api/linkedin.png')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => Linking.openURL("https://www.facebook.com/profile.php?id=100058681659669")}>
+          <Image
+            style={styles.iconStyle}
+            source={require('../api/facebook.png')}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  aboutContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+
+  imgStyle: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+  },
+  mainHeader: {
+    fontSize: 18,
+    color: "#344055",
+    textTransform: "uppercase",
+    fontWeight: "500",
+    // marginTop: 50,
+    marginTop: 40,
+    marginBottom: 10,
+    fontFamily: "Nunito_700Bold",
+  },
+  paraStyle: {
+    fontSize: 18,
+    color: "#7d7d7d",
+    paddingBottom: 30,
+    fontFamily: "WorkSans_400Regular",
+  },
+  aboutLayout: {
+    backgroundColor: "#4c5dab",
+    paddingHorizontal: 30,
+    // marginVertical: 30,
+    marginTop: 20,
+  },
+  aboutSubHeader: {
+    fontSize: 18,
+    color: "#fff",
+    textTransform: "uppercase",
+    fontWeight: "500",
+    marginVertical: 15,
+    fontFamily: "Nunito_700Bold",
+    alignSelf: "center",
+  },
+  aboutPara: {
+    color: "#fff",
+    fontSize: 18,
+    fontFamily: "WorkSans_400Regular",
+    lineHeight: 26,
+  },
+  menuContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+
+  iconStyle: {
+    width: "100%",
+    height: 50,
+    aspectRatio: 1,
+  },
+
+});
+
+export default About;
